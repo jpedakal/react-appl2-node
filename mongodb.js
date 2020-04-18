c128605842961f1d90e97e9b973e59f94ab3a4c1
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2020-04-18 12:50:50 +0530

    Update mongodb.js

--- mongodb.js
+++ mongodb.js
@@ -2,7 +2,7 @@ const { MongoClient } = require('mongodb');
 
 // database connection
 const URL = "mongodb://127.0.0.1:27017/?gssapiServiceName=mongodb";
-const dbName = "react-node3";
+const dbName = "redux-appl2";
 let db;
 
 
