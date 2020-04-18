64f93d428c74ee86acf566ee6653df98e62db2d5
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2020-04-18 14:35:22 +0530

    Update get_All_article.js

--- api/get_All_article.js
+++ api/get_All_article.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 const mongo = require('../mongodb');
 
-router.get('/artist', (req, res) => {
-    const collectionName = "artist";
+router.get('/articles', (req, res) => {
+    const collectionName = "article";
     mongo.fetchData(collectionName)
         .then(doc => res.json(doc))
         .catch(err => res.json(err))
